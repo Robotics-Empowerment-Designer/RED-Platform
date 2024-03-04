567497370e0ee7cd6df8ba0471c936e0683cc6dd
Tobias Doernbach <t.doernbach@ostfalia.de>
2024-03-04 14:20:35 +0100

    remove outdated and faulty permissions setting

--- buildContainers.sh
+++ buildContainers.sh
@@ -5,14 +5,4 @@ sudo docker build -t node-red node-red/
 
 # create data folder for Node-RED
 mkdir -p node-red/data
-#mkdir -p node-red/data-salt
-#mkdir -p node-red/data-pepper
-
-# give container write permissions
-chmod 777 node-red/data
-#chmod 777 node-red/data-salt
-#chmod 777 node-red/data-pepper
-chown 1000 node-red/data
-#chown 1000 node-red/data-salt
-#chown 1000 node-red/data-pepper
 
