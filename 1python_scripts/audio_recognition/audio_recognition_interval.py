aab977c109b8597b1ac2e7e2c1caf3e4c10f5037
Joerg Jonas Krahn <35194768+joergjonaskrahn@users.noreply.github.com>
2024-05-16 17:53:38 +0200

    changed to local rest endpoint

--- 1python_scripts/audio_recognition/audio_recognition_interval.py
+++ 1python_scripts/audio_recognition/audio_recognition_interval.py
@@ -3,12 +3,20 @@ import speech_recognition as sr
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import logging
+from flask import Flask, Response, jsonify
+from http import HTTPStatus
 
 RECORD_SECONDS = 10 # record duration
 PYAUDIO_FORMAT = pyaudio.paInt16  # Resolution: 16-bit
 CHUNK = 1024  # 2^10 buffer samples
 CHANNELS = 1
 SAMPLING_RATE = 44100
+
+app = Flask(__name__)
+logging.basicConfig(level=1)
+logger = logging.getLogger(__name__)
+
+nltk.downloader.download('vader_lexicon')
 
 class Sentiment():
     NEGATIVE = "negative"
@@ -24,6 +32,18 @@ class SentimentResults():
         self.sentiment = sentiment
         self.score = score
 
+class SentimentAnalysisResults():
+    def __init__(self, recognizedText, sentiment, score):
+        self.recognizedText = recognizedText
+        self.sentiment = sentiment
+        self.score = score
+    
+    def __json__(self):
+        return {
+            "recognizedText": self.recognizedText,
+            "sentiment": self.sentiment,
+            "score": self.score
+        }
 
 def sentiment_analysis(text):
     sid = SentimentIntensityAnalyzer()
@@ -35,56 +55,64 @@ def sentiment_analysis(text):
     
     return SentimentResults(sentiment, sentiment_score)
 
-logging.basicConfig(level=1)
-logger = logging.getLogger(__name__)
+@app.route("/sentiment-analysis", methods=["POST"])
+def speech_recognition_sentiment_analysis():
+    # Initialize PyAudioscore
+    audio = pyaudio.PyAudio()
 
-nltk.downloader.download('vader_lexicon')
+    # Initialize speech recognition
+    recognizer = sr.Recognizer()
 
-# Initialize PyAudioscore
-audio = pyaudio.PyAudio()
+    stream = audio.open(format=PYAUDIO_FORMAT, channels=CHANNELS,
+                        rate=SAMPLING_RATE, input=True,
+                        frames_per_buffer=CHUNK)
 
-# Initialize speech recognition
-recognizer = sr.Recognizer()
+    logger.info("Beginning to record ...")
 
-stream = audio.open(format=PYAUDIO_FORMAT, channels=CHANNELS,
-                    rate=SAMPLING_RATE, input=True,
-                    frames_per_buffer=CHUNK)
-
-logger.info("Beginning to record ...")
-
-frames = []
-
-try:
-    # Data recording
-    for _ in range(0, int(SAMPLING_RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-
-    logger.info("Recording finished, processing now ...")
-
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
-
-    # Create byte data out of frames
-    audio_data = b''.join(frames)
-
-    source = sr.AudioData(audio_data, SAMPLING_RATE, 2)
+    frames = []
 
     try:
-        text = recognizer.recognize_google(source)
-        logger.info(CONSOLE_COLOR.OK + f"Spoken text: {text}")
+        # Data recording
+        for _ in range(0, int(SAMPLING_RATE / CHUNK * RECORD_SECONDS)):
+            data = stream.read(CHUNK)
+            frames.append(data)
 
-        sentiment_results = sentiment_analysis(text)
-        logger.info(CONSOLE_COLOR.OK + f"Sentiment: {sentiment_results.sentiment}")
-        logger.info(CONSOLE_COLOR.OK + f"Score: {sentiment_results.score}")
-    except sr.UnknownValueError:
-        logger.error(CONSOLE_COLOR.ERROR + f"[Error] Speech could not be recognized")
-    except sr.RequestError as e:
-        logger.error(CONSOLE_COLOR.ERROR + f"[Error] RequestError: {e}")
+        logger.info("Recording finished, processing now ...")
 
-except KeyboardInterrupt:
-    logger.warn(CONSOLE_COLOR.WARNING + f"The speech recognition was manually interrupted by Keyboard Interrupt")
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
+
+        # Create byte data out of frames
+        audio_data = b''.join(frames)
+
+        source = sr.AudioData(audio_data, SAMPLING_RATE, 2)
+
+        try:
+            recognizedText = recognizer.recognize_google(source)
+            logger.info(CONSOLE_COLOR.OK + f"Spoken text: {recognizedText}")
+
+            sentiment_results = sentiment_analysis(recognizedText)
+            logger.info(CONSOLE_COLOR.OK + f"Sentiment: {sentiment_results.sentiment}")
+            logger.info(CONSOLE_COLOR.OK + f"Score: {sentiment_results.score}")
+
+            return jsonify({
+                "recognizedText": recognizedText,
+                "sentiment": sentiment_results.sentiment,
+                "score": sentiment_results.score
+            })
+        except sr.UnknownValueError:
+            logger.error(CONSOLE_COLOR.ERROR + f"[Error] Speech could not be recognized")
+            return Response(status=HTTPStatus.INTERNAL_SERVER_ERROR)
+        except sr.RequestError as e:
+            logger.error(CONSOLE_COLOR.ERROR + f"[Error] RequestError: {e}")
+            return Response(status=HTTPStatus.INTERNAL_SERVER_ERROR)
+
+    except KeyboardInterrupt:
+        logger.warn(CONSOLE_COLOR.WARNING + f"The speech recognition was manually interrupted by Keyboard Interrupt")
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
+
+if __name__ == "__main__":
+    app.run(debug = True, host="0.0.0.0", port=5728)
--- node-red/nodes/pepper/sentimentAnalysis/sentimentAnalysis.js
+++ node-red/nodes/pepper/sentimentAnalysis/sentimentAnalysis.js
@@ -1,3 +1,5 @@
+const sentiment_analysis_endpoint = "http://localhost:5728/sentiment-analysis"
+
 const SENTIMENT = Object.freeze({
     "POSITIVE": "positive",
     "NEGATIVE": "negative"
@@ -8,50 +10,45 @@ module.exports = RED => {
     const ConnectionHelper = require("../connectionHelper");
     const EventPubSub = require('node-red-contrib-base/eventPubSub');
 
-    const events = new EventPubSub();
-
     function SentimentAnalysis(config) {
         RED.nodes.createNode(this, config);
         const node = this;
-        node.path = "/robot/sentiment/analysis";
+
+        const ch = new ConnectionHelper(socket, node);
 
         let waitingNode = null;
-        const ch = new ConnectionHelper(socket, node);
 
         node.on("input", () => {
             waitingNode = msg;
             node.status({ fill: "blue", shape: "dot", text: node.type + "... recording audio ..." });
-            ch.emit();
-        });
 
-        ch.socket.on("/event/sentiment/analysis", detectedSentiment => {
-            if (!waitingNode) {
-                return;
-            }
-
-            const output = [];
-            Object.values(SENTIMENT).forEach(sentimentComp => {
-                output.push(sentimentComp === detectedSentiment);
-            });
-
-            // Falls es nicht funktioniert, ersetzen durch:
-            /*
+            fetch(sentiment_analysis_endpoint, {
+                method: "POST"
+            }).then(response => {
+                ch.emit("Test", "/robot/tablet/text");
+                /*if (!waitingNode) {
+                    return;
+                }
+    
+                const output = [];
                 Object.values(SENTIMENT).forEach(sentimentComp => {
-                    output.push(sentimentComp === detectedSentiment? true : null)
+                    output.push(sentimentComp === detectedSentiment);
                 });
-            */            
-
-            node.send(output);
-
-            node.send(waitingNode);
-            waitingNode = null;
-            
-            node.status({});
-        });
-
-        events.subscribe(EventPubSub.RESET_NODE_STATE, () => {
-            waitingNode = null;
-            node.status({});
+    
+                // Falls es nicht funktioniert, ersetzen durch:
+                
+                    Object.values(SENTIMENT).forEach(sentimentComp => {
+                        output.push(sentimentComp === detectedSentiment? true : null)
+                    });
+                           
+    
+                node.send(output);
+    
+                node.send(waitingNode);
+                waitingNode = null;
+                
+                node.status({});*/
+            })
         });
     }
 
